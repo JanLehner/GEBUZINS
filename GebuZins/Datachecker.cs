7a9708b9c10c2dd2b91ac111d3c9e3f4372e4ea1
Ellis Arn <ellisarn@gmail.com>
2023-09-24 12:38:25 +0200

    Datachecker akzeptiert zwei Nachkommastellen und "0.00" als Placeholder-Test für Zinsen und Vermögen hinzugefügt

--- GebuZins/Datachecker.cs
+++ GebuZins/Datachecker.cs
@@ -1,12 +1,29 @@
-﻿namespace GebuZins
+﻿using System.Text.RegularExpressions;
+
+namespace GebuZins
 {
     public class Datachecker
     {
         public void CheckData(object sender)
         {
             var textBox = (TextBox)sender;
-            if (!System.Text.RegularExpressions.Regex.IsMatch(textBox.Text, "[^0-9]")) return;
-            textBox.Text = System.Text.RegularExpressions.Regex.Replace(textBox.Text, "[^0-9]", "");
+            string input = textBox.Text;
+            string cleanedInput = new(input.Where(c => char.IsDigit(c) || c == '.').ToArray());
+            bool decimalEntered = cleanedInput.Contains('.');
+
+            cleanedInput = cleanedInput.Replace(".", "");
+
+            if (!decimalEntered) return;
+            cleanedInput = cleanedInput.TrimStart('0');
+            cleanedInput = cleanedInput.Length switch
+            {
+                >= 3 => cleanedInput.Insert(cleanedInput.Length - 2, "."),
+                2 => cleanedInput.Insert(0, "0."),
+                1 => cleanedInput.Insert(0, "0.0"),
+                _ => cleanedInput.Insert(0, "0.00")
+            };
+
+            textBox.Text = cleanedInput;
             textBox.SelectionStart = textBox.Text.Length;
         }
     }
--- GebuZins/Form2.Designer.cs
+++ GebuZins/Form2.Designer.cs
@@ -53,9 +53,10 @@
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = SystemColors.Control;
-            label1.Location = new Point(87, 533);
+            label1.Location = new Point(61, 320);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(120, 30);
+            label1.Size = new Size(82, 19);
             label1.TabIndex = 6;
             label1.Text = "Vermögen:";
             // 
@@ -63,9 +64,10 @@
             // 
             buttonCalculate.Anchor = AnchorStyles.None;
             buttonCalculate.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
-            buttonCalculate.Location = new Point(230, 667);
+            buttonCalculate.Location = new Point(161, 400);
+            buttonCalculate.Margin = new Padding(2, 2, 2, 2);
             buttonCalculate.Name = "buttonCalculate";
-            buttonCalculate.Size = new Size(214, 80);
+            buttonCalculate.Size = new Size(150, 48);
             buttonCalculate.TabIndex = 5;
             buttonCalculate.Text = "Bonus berechnen";
             buttonCalculate.UseVisualStyleBackColor = true;
@@ -74,18 +76,22 @@
             // textBoxInterestRate
             // 
             textBoxInterestRate.Anchor = AnchorStyles.None;
-            textBoxInterestRate.Location = new Point(230, 353);
+            textBoxInterestRate.Location = new Point(161, 212);
+            textBoxInterestRate.Margin = new Padding(2, 2, 2, 2);
             textBoxInterestRate.Name = "textBoxInterestRate";
-            textBoxInterestRate.Size = new Size(213, 31);
+            textBoxInterestRate.PlaceholderText = "0.00";
+            textBoxInterestRate.Size = new Size(150, 23);
             textBoxInterestRate.TabIndex = 8;
             textBoxInterestRate.TextChanged += CheckInputData;
             // 
             // textBoxWealth
             // 
             textBoxWealth.Anchor = AnchorStyles.None;
-            textBoxWealth.Location = new Point(230, 532);
+            textBoxWealth.Location = new Point(161, 319);
+            textBoxWealth.Margin = new Padding(2, 2, 2, 2);
             textBoxWealth.Name = "textBoxWealth";
-            textBoxWealth.Size = new Size(213, 31);
+            textBoxWealth.PlaceholderText = "0.00";
+            textBoxWealth.Size = new Size(150, 23);
             textBoxWealth.TabIndex = 9;
             textBoxWealth.TextChanged += CheckInputData;
             // 
@@ -95,9 +101,10 @@
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             label3.ForeColor = SystemColors.Control;
-            label3.Location = new Point(87, 445);
+            label3.Location = new Point(61, 267);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(129, 30);
+            label3.Size = new Size(87, 19);
             label3.TabIndex = 12;
             label3.Text = "Geburtstag:";
             // 
@@ -107,9 +114,10 @@
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             label4.ForeColor = SystemColors.Control;
-            label4.Location = new Point(87, 353);
+            label4.Location = new Point(61, 212);
+            label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(98, 30);
+            label4.Size = new Size(66, 19);
             label4.TabIndex = 13;
             label4.Text = "Zinssatz:";
             // 
@@ -119,9 +127,10 @@
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             label5.ForeColor = SystemColors.Control;
-            label5.Location = new Point(489, 353);
+            label5.Location = new Point(342, 212);
+            label5.Margin = new Padding(2, 0, 2, 0);
             label5.Name = "label5";
-            label5.Size = new Size(31, 30);
+            label5.Size = new Size(21, 19);
             label5.TabIndex = 14;
             label5.Text = "%";
             // 
@@ -131,9 +140,10 @@
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             label6.ForeColor = SystemColors.Control;
-            label6.Location = new Point(466, 533);
+            label6.Location = new Point(326, 320);
+            label6.Margin = new Padding(2, 0, 2, 0);
             label6.Name = "label6";
-            label6.Size = new Size(53, 30);
+            label6.Size = new Size(36, 19);
             label6.TabIndex = 15;
             label6.Text = "CHF";
             // 
@@ -142,10 +152,10 @@
             pickerBirthday.Anchor = AnchorStyles.None;
             pickerBirthday.CustomFormat = " dd MMMM";
             pickerBirthday.Format = DateTimePickerFormat.Custom;
-            pickerBirthday.Location = new Point(230, 442);
-            pickerBirthday.Margin = new Padding(4, 3, 4, 3);
+            pickerBirthday.Location = new Point(161, 265);
+            pickerBirthday.Margin = new Padding(3, 2, 3, 2);
             pickerBirthday.Name = "pickerBirthday";
-            pickerBirthday.Size = new Size(213, 31);
+            pickerBirthday.Size = new Size(150, 23);
             pickerBirthday.TabIndex = 17;
             pickerBirthday.Value = new DateTime(2023, 1, 1, 0, 0, 0, 0);
             // 
@@ -153,10 +163,9 @@
             // 
             pictureBox1.Anchor = AnchorStyles.None;
             pictureBox1.Image = Properties.Resources.GEBUZINS_logo;
-            pictureBox1.Location = new Point(179, 22);
-            pictureBox1.Margin = new Padding(4, 5, 4, 5);
+            pictureBox1.Location = new Point(125, 13);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(316, 122);
+            pictureBox1.Size = new Size(221, 73);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox1.TabIndex = 18;
             pictureBox1.TabStop = false;
@@ -167,10 +176,9 @@
             labelError.AutoSize = true;
             labelError.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
             labelError.ForeColor = Color.Red;
-            labelError.Location = new Point(206, 600);
-            labelError.Margin = new Padding(4, 0, 4, 0);
+            labelError.Location = new Point(144, 360);
             labelError.Name = "labelError";
-            labelError.Size = new Size(0, 28);
+            labelError.Size = new Size(0, 19);
             labelError.TabIndex = 19;
             labelError.TextAlign = ContentAlignment.MiddleLeft;
             // 
@@ -179,9 +187,10 @@
             labelAmountTax.Anchor = AnchorStyles.None;
             labelAmountTax.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             labelAmountTax.ForeColor = SystemColors.Control;
-            labelAmountTax.Location = new Point(14, 282);
+            labelAmountTax.Location = new Point(10, 169);
+            labelAmountTax.Margin = new Padding(2, 0, 2, 0);
             labelAmountTax.Name = "labelAmountTax";
-            labelAmountTax.Size = new Size(430, 28);
+            labelAmountTax.Size = new Size(301, 17);
             labelAmountTax.TabIndex = 23;
             labelAmountTax.Text = "- CHF";
             labelAmountTax.TextAlign = ContentAlignment.MiddleRight;
@@ -191,9 +200,10 @@
             labelAmountToGet.Anchor = AnchorStyles.None;
             labelAmountToGet.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             labelAmountToGet.ForeColor = SystemColors.Control;
-            labelAmountToGet.Location = new Point(14, 197);
+            labelAmountToGet.Location = new Point(10, 118);
+            labelAmountToGet.Margin = new Padding(2, 0, 2, 0);
             labelAmountToGet.Name = "labelAmountToGet";
-            labelAmountToGet.Size = new Size(430, 28);
+            labelAmountToGet.Size = new Size(301, 17);
             labelAmountToGet.TabIndex = 22;
             labelAmountToGet.Text = "- CHF";
             labelAmountToGet.TextAlign = ContentAlignment.MiddleRight;
@@ -204,9 +214,10 @@
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             label2.ForeColor = SystemColors.Control;
-            label2.Location = new Point(227, 253);
+            label2.Location = new Point(159, 152);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(215, 30);
+            label2.Size = new Size(145, 19);
             label2.TabIndex = 21;
             label2.Text = "Verrechnungssteuer:";
             // 
@@ -216,9 +227,10 @@
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
             label7.ForeColor = SystemColors.Control;
-            label7.Location = new Point(217, 167);
+            label7.Location = new Point(152, 100);
+            label7.Margin = new Padding(2, 0, 2, 0);
             label7.Name = "label7";
-            label7.Size = new Size(221, 30);
+            label7.Size = new Size(152, 19);
             label7.TabIndex = 20;
             label7.Text = "Zu erhaltener Betrag:";
             // 
@@ -226,9 +238,10 @@
             // 
             buttonNew.Anchor = AnchorStyles.None;
             buttonNew.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
-            buttonNew.Location = new Point(230, 767);
+            buttonNew.Location = new Point(161, 460);
+            buttonNew.Margin = new Padding(2, 2, 2, 2);
             buttonNew.Name = "buttonNew";
-            buttonNew.Size = new Size(214, 80);
+            buttonNew.Size = new Size(150, 48);
             buttonNew.TabIndex = 24;
             buttonNew.Text = "Neu";
             buttonNew.UseVisualStyleBackColor = true;
@@ -236,10 +249,10 @@
             // 
             // Form2
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DarkSlateGray;
-            ClientSize = new Size(674, 877);
+            ClientSize = new Size(472, 526);
             Controls.Add(buttonNew);
             Controls.Add(labelAmountTax);
             Controls.Add(labelAmountToGet);
@@ -256,6 +269,7 @@
             Controls.Add(textBoxInterestRate);
             Controls.Add(label1);
             Controls.Add(buttonCalculate);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Form2";
             Text = "Eingabe Daten";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
